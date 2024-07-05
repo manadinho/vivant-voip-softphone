a4d21117c176e9f9f459d13010bc7c6504f93a96
Muhammad Imran Israr <mimranisrar6@gmail.com>
2024-07-05 20:11:57 +0500

    made some changes

--- assets/js/renderer.js
+++ assets/js/renderer.js
@@ -1,27 +1,81 @@
-const { ipcRenderer } = require('electron');
+const { ipcRenderer } = require("electron");
 
 window.DOMAIN = null;
 
-ipcRenderer.on('phone-number', (event, phoneNumber) => {
-    const iframe = document.querySelector('#vivant-softphone-iframe');
+ipcRenderer.on("doDial", (event, phoneNumber) => {
+  const iframe = document.querySelector("#vivant-softphone-iframe");
 
-    iframe.contentWindow.postMessage({ action: 'doDial', data: { phoneNumber }  }, '*');
+  iframe.contentWindow.postMessage(
+    { action: "doDial", data: { phoneNumber } },
+    "*"
+  );
 });
 
-ipcRenderer.on('domain-info', (event, domain) => {
-    window.DOMAIN = domain;
-    setIframeSrc();
+ipcRenderer.on("set-subdomain", (event, domain) => {
+  window.DOMAIN = domain;
+  setIframeSrc();
 });
 
+// HANDLE RESET SETUP EVENT
+ipcRenderer.on("reset-setup", () => {
+  window.DOMAIN = null;
+  const iframe = document.querySelector("#vivant-softphone-iframe");
+  iframe.contentWindow.postMessage({ action: "doLogout" }, "*");
+  ipcRenderer.send("reset-setup-done", {});
+});
+
+/**
+ * Sets the domain for the softphone.
+ */
 function setDomain() {
-    const domain = document.querySelector('#domain').value;
+  const [success, subdomainOrMessage] = extractingSubDomain(
+    document.querySelector("#subdomain").value
+  );
+  if (!success) return alert(subdomainOrMessage);
 
-    if(!domain) return alert('Please enter a domain');
-
-    ipcRenderer.send('configure-domain', domain);
+  ipcRenderer.send("configure-subdomain", subdomainOrMessage);
 }
 
+/**
+ * Sets the source URL of the iframe to display the softphone.
+ */
 function setIframeSrc() {
-    const url = `https://fusionapi.vivantvoip.dev/softphone?subdomain=${window.DOMAIN}`;
-    document.querySelector('#vivant-softphone-iframe').src = url;
+  const url = `https://fusionapi.vivantvoip.dev/softphone?subdomain=${window.DOMAIN}`;
+  const iframe = document.querySelector("#vivant-softphone-iframe");
+  if (!iframe) return;
+  iframe.src = url;
+}
+
+/**
+ * Extracts the subdomain from a given URL or validates if the input is a valid subdomain.
+ * @param {string} input - The URL or subdomain input.
+ * @returns {Array} - An array containing a boolean indicating if the subdomain is present, and the extracted subdomain or an error message.
+ */
+function extractingSubDomain(input) {
+  try {
+    // Try to construct a URL object; this throws if `input` is not a valid URL
+    const urlObject = new URL(input);
+    const hostname = urlObject.hostname;
+
+    // Assuming domain format like "example.domain.com"
+    const parts = hostname.split(".");
+
+    // Assuming the base domain always consists of two parts (e.g., "domain.com")
+    if (parts.length > 2) {
+      parts.pop(); // Remove TLD
+      parts.pop(); // Remove domain
+      const subdomain = parts.join(".");
+      return [true, subdomain];
+    } else {
+      return [false, "No Subdomain present"];
+    }
+  } catch (error) {
+    // If the URL constructor fails, assume `input` is a subdomain or an invalid URL
+    // Validate if input could plausibly be a subdomain
+    if (/^[a-zA-Z0-9\-\.]+$/.test(input)) {
+      return [true, input]; // Input looks like a valid subdomain
+    } else {
+      return [false, "Input is neither a valid URL nor a valid subdomain"];
+    }
+  }
 }
--- main.js
+++ main.js
@@ -1,78 +1,89 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, globalShortcut } = require("electron");
 
 let mainWindow;
 let store;
 
 // Dynamically import electron-store and initialize the store after import
-import('electron-store').then(ElectronStore => {
+import("electron-store")
+  .then((ElectronStore) => {
     store = new ElectronStore.default(); // Ensure that you are accessing the default export correctly
 
     app.whenReady().then(createWindow);
-}).catch(e => {
-    console.error('Failed to load electron-store:', e);
-});
+  })
+  .catch((e) => {
+    console.error("Failed to load electron-store:", e);
+  });
 
 function createWindow() {
-    app.setName('Vivant VoIP');
+  app.setName("Vivant VoIP");
 
-    mainWindow = new BrowserWindow({
-        width: 345,
-        height: 605,
-        icon: __dirname + '/assets/images/pbx.icns',
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-        },
-        resizable: false,
-        fullscreenable: false,
+  mainWindow = new BrowserWindow({
+    width: 345,
+    height: 605,
+    icon: __dirname + "/assets/images/pbx.icns",
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+    resizable: false,
+    fullscreenable: false,
+  });
+
+  // Load setup wizard or main app depending on whether subdomain is configured
+  // store.clear();
+  const subdomain = store.get("subdomain");
+  if (!subdomain) {
+    mainWindow.loadURL("file://" + __dirname + "/setup.html");
+  } else {
+    mainWindow.loadURL("file://" + __dirname + "/index.html");
+
+    // Send subdomain info to the renderer process
+    mainWindow.webContents.on("did-finish-load", () => {
+      mainWindow.webContents.send("set-subdomain", subdomain);
     });
+  }
 
-    // Load setup wizard or main app depending on whether domain is configured
-    // store.clear();
-    const domain = store.get('domain');
-    if (!domain) {
-        mainWindow.loadURL('file://' + __dirname + '/setup.html');
-    } else {
-        mainWindow.loadURL('file://' + __dirname + '/index.html');
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
 
-        // Send domain info to the renderer process
-        mainWindow.webContents.on('did-finish-load', () => {
-            mainWindow.webContents.send('domain-info', domain);
-        });
-    }
-
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+  globalShortcut.register("CommandOrControl+D", () => {
+    mainWindow.webContents.send("reset-setup", {});
+    store.clear();
+  });
 }
 
-ipcMain.on('configure-domain', (event, domain) => {
-    store.set('domain', domain);
-    mainWindow.loadURL('file://' + __dirname + '/index.html');
-    
-    mainWindow.webContents.on('did-finish-load', () => {
-        mainWindow.webContents.send('domain-info', domain);
-    });
+ipcMain.on("configure-subdomain", (event, subdomain) => {
+  store.set("subdomain", subdomain);
+  mainWindow.loadURL("file://" + __dirname + "/index.html");
+
+  mainWindow.webContents.on("did-finish-load", () => {
+    mainWindow.webContents.send("set-subdomain", subdomain);
+  });
+});
+
+ipcMain.on("reset-setup-done", () => {
+  mainWindow.loadURL("file://" + __dirname + "/setup.html");
 });
 
 // Ensure the app is the default client for 'tel' links
-app.setAsDefaultProtocolClient('tel');
+app.setAsDefaultProtocolClient("tel");
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
 });
 
-app.on('open-url', (event, url) => {
-    event.preventDefault();
-    const phoneNumber = url.split(':')[1];
-    mainWindow.webContents.send('phone-number', phoneNumber);
-    if (mainWindow.isMinimized()) mainWindow.restore();
-    mainWindow.focus();
+app.on("open-url", (event, url) => {
+  event.preventDefault();
+  const phoneNumber = url.split(":")[1];
+  mainWindow.webContents.send("doDial", phoneNumber);
+  if (mainWindow.isMinimized()) mainWindow.restore();
+  mainWindow.focus();
 });
